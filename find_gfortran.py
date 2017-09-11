e3742955a57abca5eee441645f2713270ba1d2c1
Danny Hermes <daniel.j.hermes@gmail.com>
2017-09-10 22:52:23 -0700

    Adding custom script to find gfortran on AppVeyor.

---
+++ find_gfortran.py
@@ -0,0 +1,22 @@
+PATHS = (
+    r'C:\MinGW\bin',
+    r'C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0',
+    r'C:\mingw-w64\i686-6.3.0-posix-dwarf-rt_v5-rev1',
+    r'C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1',
+)
+
+def find_gfortran(root):
+    for subdir, _, filenames in os.walk(root):
+        for filename in filenames:
+            if 'gfortran' in filename.lower():
+                yield os.path.join(subdir, filename)
+
+
+def main():
+    for path in PATHS:
+        for match in find_gfortran(path):
+            print(match)
+
+
+if __name__ == '__main__':
+    main()
