444f1d1687bb5da4303e0e8105c51f222ec14e4f
Danny Hermes <daniel.j.hermes@gmail.com>
2017-09-11 13:02:06 -0700

    Updating `bezier` with custom `obj` filenames.
    Also adding a little bit to `find_gfortran` to show what
    happens when `setup.get_f90_compiler()` is called from
    `bezier/`.

--- find_gfortran.py
+++ find_gfortran.py
@@ -1,3 +1,4 @@
+import imp
 import os
 
 
@@ -38,6 +39,27 @@ def which_gfortran():
                 print(potential_match)
 
 
+def setup_info():
+    # Print some information about the Fortran compiler
+    setup_filename = os.path.join(
+        os.path.dirname(__file__),
+        'bezier',
+        'setup.py',
+    )
+    setup_mod = imp.load_source('setup', setup_filename)
+    f90_compiler = setup_mod.get_f90_compiler()
+    if f90_compiler is None:
+        print('f90_compiler = None')
+        print('f90_exe      = None')
+        print('c_compiler   = None')
+    else:
+        print('f90_compiler = {}'.format(f90_compiler))
+        f90_exe = f90_compiler.compiler_f90[0]
+        print('f90_exe      = {}'.format(f90_exe))
+        c_compiler = f90_compiler.c_compiler
+        print('c_compiler   = {}'.format(c_compiler))
+
+
 def main():
     for path in PATHS:
         for match in find_gfortran(path):
@@ -51,6 +73,7 @@ def main():
             print('{}={}'.format(env_var, value))
 
     which_gfortran()
+    setup_info()
 
 
 if __name__ == '__main__':
