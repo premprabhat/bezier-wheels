1961188437090cbe51b9cd03b93bddf9bb0e2da9
Danny Hermes <daniel.j.hermes@gmail.com>
2017-09-11 08:52:16 -0700

    Adding some more investigative snippets to AppVeyor config.

--- find_gfortran.py
+++ find_gfortran.py
@@ -7,6 +7,10 @@ PATHS = (
     r'C:\mingw-w64\i686-6.3.0-posix-dwarf-rt_v5-rev1',
     r'C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1',
 )
+ENV_VARS = (
+    'PATH',
+    'PATHEXT',
+)
 
 
 def find_gfortran(root):
@@ -16,10 +20,37 @@ def find_gfortran(root):
                 yield os.path.join(subdir, filename)
 
 
+def which_gfortran():
+    path_extension = os.environ.get('PATHEXT', '')
+    valid_extensions = [
+        extension
+        for extension in path_extension.split(os.pathsep)
+        if extension
+    ]
+
+    path = os.environ.get('PATH', '')
+    path_directories = path.split(os.pathsep)
+    for directory_on_path in path_directories:
+        basename = os.path.join(directory_on_path, 'gfortran')
+        for extension in valid_extensions:
+            potential_match = basename + extension
+            if os.access(potential_match, os.X_OK):
+                print(potential_match)
+
+
 def main():
     for path in PATHS:
         for match in find_gfortran(path):
             print(match)
+
+    for env_var in ENV_VARS:
+        value = os.environ.get(env_var)
+        if value is None:
+            print('{} is unset'.format(env_var))
+        else:
+            print('{}={}'.format(env_var, value))
+
+    which_gfortran()
 
 
 if __name__ == '__main__':
