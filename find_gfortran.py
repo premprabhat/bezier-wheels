7b919b6384fdc4c376d615133d81dda40c276e0c
Danny Hermes <daniel.j.hermes@gmail.com>
2017-09-10 22:54:01 -0700

    Adding missing import to find_gfortran.

--- find_gfortran.py
+++ find_gfortran.py
@@ -1,9 +1,13 @@
+import os
+
+
 PATHS = (
     r'C:\MinGW\bin',
     r'C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0',
     r'C:\mingw-w64\i686-6.3.0-posix-dwarf-rt_v5-rev1',
     r'C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1',
 )
+
 
 def find_gfortran(root):
     for subdir, _, filenames in os.walk(root):
