1c510242db4c2d3dc087fd2f7000923a53499131
Danny Hermes <daniel.j.hermes@gmail.com>
2020-02-03 23:23:42 -0800

    Pinning to `cmake == 3.13.2` for 32-bit Linux in `manylinux2010`.
    This worked last week but broke as of today's release (3.16.3)
    of `cmake` on PyPI.

--- travis/config.sh
+++ travis/config.sh
@@ -25,7 +25,7 @@ function pre_build {
     else
         # NOTE: `3.13.2` is the last version of `cmake` that supports 32-bit
         #       linux.
-        /opt/python/cp37-cp37m/bin/python -m pip install "cmake >= 3.13.2"
+        /opt/python/cp37-cp37m/bin/python -m pip install "cmake == 3.13.3"
         cp /opt/python/cp37-cp37m/bin/cmake /usr/bin/cmake
         cd /io/bezier
     fi
