4a9e3c71250fac837bc243aa1b5fbf64ece38a4b
Danny Hermes <daniel.j.hermes@gmail.com>
2020-02-05 22:53:38 -0800

    Describing C:\MinGW too.

--- mingw_info.py
+++ mingw_info.py
@@ -1,13 +1,21 @@
 import pathlib
 
 
-def main():
-    mingw_w64 = pathlib.Path(r"C:\mingw-w64")
-    print(mingw_w64)
-
+def describe(directory):
+    as_path = pathlib.Path(directory)
+    print(as_path)
     print("-" * 60)
-    for contained in mingw_w64.glob("*"):
-        print(repr(contained))
+    for contained in as_path.glob("*"):
+        if contained.is_dir():
+            print(repr(contained))
+        else:
+            print(repr(contained))
+
+
+def main():
+    describe(r"C:\mingw-w64")
+    print("*" * 60)
+    describe(r"C:\MinGW")
 
 
 if __name__ == "__main__":
