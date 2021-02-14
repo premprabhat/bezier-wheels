26db74a4b9dff7d205b8fa6068ee4ea08fb5c769
Danny Hermes <daniel.j.hermes@gmail.com>
2021-02-13 21:34:03 -0600

    Faking out Travis env.

--- travis/full.sh
+++ travis/full.sh
@@ -2,6 +2,13 @@
 
 set -e -x
 
+# Fake out any Travis env. vars. needed.
+if [[ "${GITHUB_ACTIONS_RUNS_ON}" == "macos-latest" ]]; then
+  export TRAVIS_OS_NAME=osx
+else
+  export TRAVIS_OS_NAME=linux
+fi
+
 source multibuild/common_utils.sh
 source multibuild/travis_steps.sh
 before_install
